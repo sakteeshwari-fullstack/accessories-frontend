a2d9f5bacc4d6e26c263b8beab54dfd1cf6aaccf
AkilanRittz <akilan@rittzdigital.com>
2025-08-08 02:03:16 +0530

    blog static completed

---
+++ app/blog/blogPage.jsx
@@ -0,0 +1,129 @@
+
+import React from "react";
+
+export default function Blog() {
+
+    return (
+        <div>
+
+           <div className="relaitve  w-full min-w-full py-10  justify-center ">       
+                <div className="relative  items-start justify-center  whitespace-nowrap flex flex-col lg:flex-row gap-[4vw]">
+                <img src="/assets/blog/Div [ratio].png" alt="" className="w-[90vw] sm:w-[60vw] md:w-[60vw] lg:w-[40vw] h-[30vh] lg:h-[55vh]" />
+                <div className="relative flex flex-col w-[50vw] bg-blackyy leading-[1.2] text-[0.9vw]">
+                     <p className="text-orange-600 text-[5vw] lg:text-[1.3vw]  ">THE BASICS</p>
+                    <div className=" flex transform -translate-y-[-1rem] lg:-translate-y-[-2rem]">
+                        <p className="text-black text-[6vw] lg:text-[2vw] font-semibold">10 Must-Attend Real Estate <span className="block">Conferences & Events in 2025</span> </p>
+                    </div>
+                    <div className="relative  mt-8 lg:mt-[60px] flex flex-row gap-1 items-center ">
+                        <img src="/assets/blog/tabler_clock.svg" alt="" className="w-[8vw] h-[3vh] sm:w-[2rem]" />
+                        <p className="text-[#404040] text-[3vw] lg:text-[1.2vw]">March 4, 2025</p>
+                    </div>
+                    <div className="mt-6 w-[90vw] lg:w-[50vw]">
+                        <p className="text-[#404040] whitespace-normal text-[4vw] lg:text-[1.5vw]">Discover the top real estate conferences in 2025 to network, gain industry insights,and stay ahead. Don’t miss these opportunities!
+                            </p>
+                        <p className="text-black mt-[8vw] font-bold text-[4vw] lg:text-[1vw]">Keep reading →</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div className="relative transform -translate-y-[1rem] lg:-translate-y-[1rem] -translate-x-[-1rem] lg:-translate-x-[-4rem] flex flex-col gap-2 w-[50vw]">   
+                <p className="text-[#404040] text-[4.5vw] lg:text-[2vw] font-bold ">The Latest</p>
+                <div className="relative mt-[1vw] lg:mt-[20] ml-[4vw]   whitespace-nowrap flex flex-col lg:flex-row gap-6 lg:gap-18 ">
+                    <div className="flex flex-col gap-4 w-[40vw]">
+                          <p className="text-[#404040] text-[3vw] lg:text-[1vw]">April 8, 2025 <span className="text-[#FF4A23] ml-2">The Basics</span></p>
+                    <p className="text-black font-bold text-[3vw] lg:text-[1vw]">The Complete Guide to Real Estate <span className="block">Referral Fees for 2025</span> </p>
+                    <p className="text-[#404040]  text-[2.5vw] lg:text-[1.3vw]">Let's remove some of the mystery around real estate referrals <span className="block">and the associated fees. We’ll review why referrals happen, </span><span className="block">explore how agreements work and offer you a template to get </span><span className="block">started.</span></p>             
+                    </div>
+                     <img src="/assets/blog/Div [w-100].png" alt="" className="relative cursor-pointer w-[55vw] lg:w-[28vw] h-[40vw] lg:h-[40vh] ml-[10vw] lg:ml-[0] "/>
+                </div>
+                <div className="relative w-65 h-px s:h-px lg:h-px  sm:w-20 md:w-50 transform -translate-x-[0rem] lg:-translate-x-[-6rem] lg:w-[70vw] bg-black" />
+                  <div className="relative mt-[1vw] lg:mt-[20] ml-[4vw]   whitespace-nowrap flex flex-col lg:flex-row gap-6 lg:gap-18 ">
+                    <div className="flex flex-col gap-4 w-[40vw]">
+                          <p className="text-[#404040] text-[3vw] lg:text-[1vw]">April 8, 2025 <span className="text-[#FF4A23] ml-2">The Basics</span></p>
+                    <p className="text-black font-bold text-[3vw] lg:text-[1vw]">The Complete Guide to Real Estate <span className="block">Referral Fees for 2025</span> </p>
+                    <p className="text-[#404040]  text-[2.5vw] lg:text-[1.3vw]">Let's remove some of the mystery around real estate referrals <span className="block">and the associated fees. We’ll review why referrals happen, </span><span className="block">explore how agreements work and offer you a template to get </span><span className="block">started.</span></p>             
+                    </div>
+                     <img src="/assets/blog/Div [w-100].png" alt="" className="relative cursor-pointer w-[55vw] lg:w-[28vw] h-[40vw] lg:h-[40vh] ml-[10vw] lg:ml-[0] "/>
+                </div>
+                <div className="relative w-65 h-px s:h-px lg:h-px  sm:w-20 md:w-50 transform -translate-x-[0rem] lg:-translate-x-[-6rem] lg:w-[70vw] bg-black" />
+                
+                 <div className="relative mt-[1vw] lg:mt-[20] ml-[4vw]   whitespace-nowrap flex flex-col lg:flex-row gap-6 lg:gap-18 ">
+                    <div className="flex flex-col gap-4 w-[40vw]">
+                          <p className="text-[#404040] text-[3vw] lg:text-[1vw]">April 8, 2025 <span className="text-[#FF4A23] ml-2">The Basics</span></p>
+                    <p className="text-black font-bold text-[3vw] lg:text-[1vw]">The Complete Guide to Real Estate <span className="block">Referral Fees for 2025</span> </p>
+                    <p className="text-[#404040]  text-[2.5vw] lg:text-[1.3vw]">Let's remove some of the mystery around real estate referrals <span className="block">and the associated fees. We’ll review why referrals happen, </span><span className="block">explore how agreements work and offer you a template to get </span><span className="block">started.</span></p>             
+                    </div>
+                     <img src="/assets/blog/Div [w-100].png" alt="" className="relative cursor-pointer w-[55vw] lg:w-[28vw] h-[40vw] lg:h-[40vh] ml-[10vw] lg:ml-[0] "/>
+                </div>
+                <div className="relative w-65 h-px s:h-px lg:h-px  sm:w-20 md:w-50 transform -translate-x-[0rem] lg:-translate-x-[-6rem] lg:w-[70vw] bg-black" />
+
+                    <div className="relative flex flex-col gap-6   ">
+                        <div>
+                        <p className="text-[#FF4A23] font-semibold text-[2vw]">The Basics</p>
+                    </div>
+                    <div className="relative grid grid-cols-2 ml-[2vw] lg:grid-cols-3 gap-x-10 gap-y-6 w-[80vw] lg:w-[80vw]">
+                       <div className="relative flex flex-col gap-4">
+                        <div>
+                            <img src="/assets/blog/Div [ratio] (1).png" alt="" className="relative w-[50vw] lg:w-[20vw]"/>
+                        </div>
+                        <div className="relative whitespace-nowrap ">
+                            <p className="text-black text-[2.2vw] lg:text-[1.2vw] font-bold">Best Real Estate Company to Work <span className="block">For in 2025</span></p>
+                        </div>
+                    </div>
+                    <div className="relative flex flex-col gap-4">
+                        <div>
+                            <img src="/assets/blog/Div [ratio] (1).png" alt="" className="w-[50vw] lg:w-[20vw]"/>
+                        </div>
+                        <div className="relative whitespace-nowrap">
+                            <p className="text-black  text-[2vw] lg:text-[1.2vw] font-bold">Best Real Estate Company to Work <span className="block">For in 2025</span></p>
+                        </div>
+                    </div>
+                    <div className="relative flex flex-col gap-4 ">
+                        <div>
+                            <img src="/assets/blog/Div [ratio] (1).png" alt="" className="w-[50vw] lg:w-[20vw]"/>
+                        </div>
+                        <div className="relative whitespace-nowrap">
+                            <p className="text-black text-[2vw] lg:text-[1.2vw] font-bold">Best Real Estate Company to Work <span className="block">For in 2025</span></p>
+                        </div>
+                    </div>
+                     <div className="relative flex flex-col gap-4 ">
+                        <div>
+                            <img src="/assets/blog/Div [ratio] (1).png" alt="" className="w-[50vw] lg:w-[20vw]"/>
+                        </div>
+                        <div className="relative whitespace-nowrap">
+                            <p className="text-black text-[2vw] lg:text-[1.2vw] font-bold">Best Real Estate Company to Work <span className="block">For in 2025</span></p>
+                        </div>
+                    </div>
+                    <div className="relative flex flex-col gap-4">
+                        <div>
+                            <img src="/assets/blog/Div [ratio] (1).png" alt="" className="w-[50vw] lg:w-[20vw]"/>
+                        </div>
+                        <div className="relative whitespace-nowrap">
+                            <p className="text-black  text-[2vw] lg:text-[1.2vw] font-bold">Best Real Estate Company to Work <span className="block">For in 2025</span></p>
+                        </div>
+                    </div>
+                     <div className="relative flex flex-col gap-4">
+                        <div>
+                            <img src="/assets/blog/Div [ratio] (1).png" alt="" className="w-[50vw] lg:w-[20vw]"/>
+                        </div>
+                        <div className="relative whitespace-nowrap">
+                            <p className="text-black  text-[2vw] lg:text-[1.2vw] font-bold">Best Real Estate Company to Work <span className="block">For in 2025</span></p>
+                        </div>
+                    </div>
+ 
+ 
+                    </div>
+
+                     
+                    <div className="w-[80vw] h-px s:h-px lg:h-px  sm:w-[20vw] md:w-[50vw]  lg:w-[75vw] ml-[2vw] bg-[#00000080]" />
+
+                    </div>
+                    
+                
+            </div>
+
+
+
+        </div>
+    )
+}
---
+++ app/blog/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Blog from "./blogPage.jsx";
+
+
+export default function page() {
+    return(
+  <div className="restrict w-full h-auto font-poppins bg-[#F9FAFB]">
+        <Blog />
+        </div>
+    )
+}
